116a6a2d69facfdfeba0d8dea0e18e0e712da58b
Serjjy <danilin.vladislav6032@gmail.com>
2026-06-21 10:07:30 +0300

    Add files via upload

---
+++ jsgallery.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll(".gallery-img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.getElementById("close");
+
+images.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        modal.style.display = "flex";
+        modalImg.src = img.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
+
+modal.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
